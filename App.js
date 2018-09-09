bb1976cad9c1f78cf5fbb35b8736f65c72fad8ef
Daniel Encina <dani.encina@alumnos.duoc.cl>
2018-09-09 20:31:27 -0300

    creation proyect

---
+++ App.js
@@ -0,0 +1,44 @@
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+
+        </View>
+      </Provider>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
