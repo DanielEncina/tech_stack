ce595ba9ff71b80a73a231922c3b175567e6de2b
Daniel Encina <dani.encina@alumnos.duoc.cl>
2018-09-11 20:59:51 -0300

    adding reducer to combine

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import LibraryReducers from './LibraryReducers.js';
+import SelectionReducer from './SelectionsReducer.js';
 
 export default combineReducers({
-  libraries: () => []
+  libraries: LibraryReducers,
+  selectorLibraryId: SelectionReducer
 });
